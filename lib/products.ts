280e18331ef69fcdef630d71c61f3d6865b7eaf3
mjul23 <103218872+mjul23@users.noreply.github.com>
2022-04-07 22:45:48 +0200

    Update products.ts

--- lib/products.ts
+++ lib/products.ts
@@ -1,18 +1,18 @@
 export const products = [
   {
-    id: 'box-of-cookies',
+    id: 'box-of-cook',
     name: 'Box',
     description: 'A delicious box of handmade cookies',
     unitName: 'box', // shows after the price, eg. 0.05 SOL/box
     priceSol: 0.05,
-    priceUsd: 5,
+    priceUsd: 3,
   },
   {
-    id: 'basket-of-cookies',
+    id: 'basket-of-cook',
     name: 'Basket',
     description: 'A large basket of handmade cookies',
     unitName: 'basket',
     priceSol: 0.1,
-    priceUsd: 10,
+    priceUsd: 6,
   }
 ]
